c5a82377858ae075004e7500652e4f37abcfc5dc
playforge006-ui <playforge006@gmail.com>
2025-09-24 06:41:20 +0530

    Fix DialogTitle accessibility by removing VisuallyHidden wrapper

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { type DialogProps } from "@radix-ui/react-dialog";
 import { Command as CommandPrimitive } from "cmdk";
-import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 import { Search } from "lucide-react";
 
 import { cn } from "@/lib/utils";
@@ -28,9 +27,7 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
       <DialogContent className="overflow-hidden p-0 shadow-lg">
-        <VisuallyHidden.Root>
-          <DialogTitle>Command Menu</DialogTitle>
-        </VisuallyHidden.Root>
+        <DialogTitle className="sr-only">Command Menu</DialogTitle>
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
